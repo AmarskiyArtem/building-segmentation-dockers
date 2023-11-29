aab1030bc92e3262362dc1524ee653b5891254e1
user <amarskiywork@gmail.com>
2023-11-30 00:13:31 +0300

    add getting paths from args

--- rgb-footprint-extract/run.py
+++ rgb-footprint-extract/run.py
@@ -4,13 +4,12 @@ import os
 
 def main():
     args = sys.argv
-    npy_dir = '/home/user/programming/squareTest/2_npy/'
-    #npy_folder = args[1][:-1] + '_npy/'
-    input_dir = '/home/user/programming/squareTest/2/'
-    output_dir = '/home/user/programming/new_test/'
+    input_dir = args[1]
+    npy_dir = args[1][:-1] + '_npy/'
+    output_dir = args[2]
     adapter.convert_images_in_dir(input_dir, npy_dir)
-    os.system('bash run.sh {npy_dir} {output_dir}')
-    print('ok')
+    os.system(f'bash run.sh {npy_dir} {output_dir}')
+
 
 if __name__ == '__main__':
     main()
