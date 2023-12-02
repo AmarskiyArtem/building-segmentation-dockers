1a20eb1f8fe05451f8362ebf172bd7b178cf21e8
user <amarskiywork@gmail.com>
2023-12-02 15:35:49 +0300

    Change folder structure, add working Dockerfile

--- rgb-footprint-extract/run.py
+++ rgb-footprint-extract/run.py
@@ -8,7 +8,7 @@ def main():
     npy_dir = args[1][:-1] + '_npy/'
     output_dir = args[2]
     adapter.convert_images_in_dir(input_dir, npy_dir)
-    os.system(f'bash run.sh {npy_dir} {output_dir}')
+    os.system(f'bash ./rgb-footprint-extract/run.sh {npy_dir} {output_dir}')
 
 
 if __name__ == '__main__':
