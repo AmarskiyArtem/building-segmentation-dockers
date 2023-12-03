fc6d66b257debb240944fb284fc1901e9163db8b
user <amarskiywork@gmail.com>
2023-12-03 13:16:22 +0300

    Add abstact adapter class

--- common/adapter/adapter_base.py
+++ common/adapter/adapter_base.py
@@ -3,9 +3,33 @@ from abc import ABC, abstractmethod
 
 class AdapterBase(ABC):
 
-    def __init__(self, input_dir, output_dir):
-        self.input_dir = input_dir
-        self.output_dir = output_dir
+    def __init__(self, input_path, output_path):
+        self.input_path = input_path
+        self.output_path = output_path
+
+    @abstractmethod
+    def preprocessing(self):
+        pass
+
+    @abstractmethod
+    def convert_to_input_format(self):
+        pass
+
+    @abstractmethod
+    def predict_masks(self):
+        pass
+
+    @abstractmethod
+    def convert_to_output_format(self):
+        pass
+
+    @abstractmethod
+    def postprocessing(self):
+        pass
 
     def run(self):
-        self._convert_to_input_format()
+        self.preprocessing()
+        self.convert_to_input_format()
+        self.predict_masks()
+        self.convert_to_output_format()
+        self.postprocessing()
--- rgb-footprint-extract/adapter.py
+++ rgb-footprint-extract/adapter.py
@@ -1,13 +1,18 @@
-from PIL import Image
 import numpy as np
 import os
 
-class Adapter:
+from PIL import Image
+
+from common.adapter.adapter_base import AdapterBase
+
+class Adapter(AdapterBase):
 
     def __init__(self, input_path, output_path):
-        self.input_path = input_path
-        self.output_path = output_path
+        super().__init__(input_path, output_path)
         
+    def preprocessing(self):
+        pass
+    
     def save_image_as_npy(self, file, npy_path):
         filename, file_extension = os.path.splitext(os.path.basename(file))
         image = Image.open(self.input_path + file).convert('RGB')
@@ -16,11 +21,19 @@ class Adapter:
             os.makedirs(npy_path)
         np.save(output, image)
     
-    def convert_images_in_dir(self):
+    def convert_to_input_format(self):
         for file in os.listdir(self.input_path):
             self.save_image_as_npy(file, self.input_path[:-1] + '_npy/')
 
-    def run(self):
-        self.convert_images_in_dir()
+    def predict_masks(self):
         os.chdir('/rgb-footprint-extract/rgb-footprint-extract/')
         os.system(f'bash run.sh {self.input_path[:-1]}_npy/ {self.output_path}/')
+
+    def convert_to_output_format(self):
+        pass
+
+    def postprocessing(self):
+        pass
+
+    def run(self):
+        super().run()
