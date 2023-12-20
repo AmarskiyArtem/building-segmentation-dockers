f0fc82697820251aed6793b7d25df669351b6bfb
Artem Amarskiy <113232275+AmarskiyArtem@users.noreply.github.com>
2023-12-20 19:46:30 +0300

    Update run.py

--- rgbfootprint/run.py
+++ rgbfootprint/run.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import argparse
 import adapter
 
@@ -30,18 +31,40 @@ if __name__ == "__main__":
         help="Path to the segmentation masks",
         default="/rgbfootprint/rgb_footprint_extract/output",
     )
-    parser.add_argument('--window-size', type=int, default=None, help="the size of grid blocks to sample from the input, use if encountering OOM issues")
-    parser.add_argument('--stride', type=int, default=None, help="the stride at which to sample grid blocks, recommended value is equal to `window_size`")
-    parser.add_argument('--no-cuda', action='store_true', default=
-                        False, help='disables CUDA training')
-    parser.add_argument('--gpu-ids', type=str, default='0',
-                        help='use which gpu to train, must be a \
-                        comma-separated list of integers only (default=0)')
-    parser.add_argument('--backbone', type=str, default='drn_c42',
-                        choices=['resnet', 'xception', 'drn', 'mobilenet', 'drn_c42'],
-                        help='backbone name (default: resnet)')
-    parser.add_argument('--out-stride', type=int, default=8,
-                        help='network output stride (default: 8)')
-    parser.add_argument('--resume', type=str, default='crowdAI', help='experiment to load (default: crowdAI)')
+    parser.add_argument(
+        "--window-size",
+        type=int,
+        default=None,
+        help="the size of grid blocks to sample from the input, use if encountering OOM issues",
+    )
+    parser.add_argument(
+        "--stride",
+        type=int,
+        default=None,
+        help="the stride at which to sample grid blocks, recommended value is equal to `window_size`",
+    )
+    parser.add_argument(
+        "--no-cuda", action="store_true", default=False, help="disables CUDA training"
+    )
+    parser.add_argument(
+        "--gpu-ids", type=str, default="0", help="use which gpu to train, must be a \
+                        comma-separated list of integers only (default=0)"
+    )
+    parser.add_argument(
+        "--backbone",
+        type=str,
+        default="drn_c42",
+        choices=["resnet", "xception", "drn", "mobilenet", "drn_c42"],
+        help="backbone name (default: resnet)",
+    )
+    parser.add_argument(
+        "--out-stride", type=int, default=8, help="network output stride (default: 8)"
+    )
+    parser.add_argument(
+        "--resume",
+        type=str,
+        default="crowdAI",
+        help="experiment to load (default: crowdAI)",
+    )
     args = parser.parse_args()
     adapter.Adapter(args.input_path, args.output_path, args).run()
